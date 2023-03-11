7b09863fa191bbd67ca0e7160ffe4b9714a2b0ba
xinlan <706592143@qq.com>
2023-03-11 16:45:59 +0800

    feat:init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
       <router-link to="/v-viewer">V-viewer</router-link> |
       <router-link to="/practice_model">model选项</router-link> |
       <router-link to="/practice_grid">grid栅格布局</router-link> |
